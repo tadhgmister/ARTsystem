b395dc983fa5c3092a54db203666160f37d8b590
Tadhg McDonald-Jensen <tadhg.mcdonaldjensen@carleton.ca>
2019-11-06 12:51:14 -0500

    moved vehiclePi related files into subfolder

---
+++ vehiclePi/vehicleController.py
@@ -0,0 +1,86 @@
+from __future__ import annotations
+"""
+controller for the vehicle program to move the car.
+"""
+import typing
+from common import Position, MOVE
+from towerCommunication import sync_position
+
+class Controller:
+    """
+This class maintains the position and orientation of the vehicle and
+contains methods to move the motors in order to move the car."""
+    #TODO: get actual increments for these.
+    WHEEL_STEP_INCREMENT: typing.ClassVar[float] = 0.5
+    "mm wheel moves per step."
+    WHEEL_TURN_INCREMENT: typing.ClassVar[float] = 0.1
+    "radians turned when turning wheel"
+    position: Position = None
+    "current position of the car relative to the tower, TODO: initial value?"
+    drawing: bool = None #initially not True or False so that the shortcut in set_chalk will never ignore the first instruction.
+    "whether the chalk is down so vehicle is drawing or not."
+    def __init__(self):
+        # should this take position as input or do we even know where the car is at this point?
+        NotImplemented
+
+    def correct_actual_position(self, current_step: int) -> None:
+        """
+        requests tower to find the car's exact position and updates own internal x,y,facing variables
+        this will make a call to the tower and block all operations until tower returns with answer
+        it will also send the current location (calculated based on ideal conditions) for logging purposes
+        """
+        self.position = sync_position(self.position, current_step)
+        
+    def move(self, instruction):
+        self._fail_if_not_initialized()
+        if instruction is MOVE.F:
+            #TODO: send arduino command
+            self.position = self.position.moved_forward(self.WHEEL_STEP_INCREMENT)
+        elif instruction is MOVE.R:
+            #TODO: send arduino command
+            self.position = self.position.turned( - self.WHEEL_TURN_INCREMENT)
+        elif instruction is MOVE.L:
+            #TODO: send arduino command
+            self.position = self.position.turned(self.WHEEL_TURN_INCREMENT)
+        else:
+            raise NotImplementedError(f"invalid movement: {instruction!r}")
+        
+    def _fail_if_not_initialized(self):
+        if self.position is None:
+            raise Exception("tried to move car before initializing position")
+        
+    def move_forward(self) -> None:
+        """ sends movement control to arduino to move forward
+        then update own x,y coordinates to match
+        """
+        self.move(MOVE.F)
+
+    def turn_left(self) -> None:
+        """ sends motor control to arduino to turn in place CCW
+        updates facing variable to match
+        """
+        self.move(MOVE.L)
+
+    def turn_right(self) -> None:
+        """ sends motor control to arduino to turn in place CW
+        updates facing variable to match
+        """
+        self.move(MOVE.R)
+
+    def forward_right(self) -> None: #typing.NoReturn
+        """ moves left wheel forward one increment to turn right"""
+        raise NotImplementedError("not expected to use?")
+    def forward_left(self) -> None: #typing.NoReturn
+        """ moves right wheel forward one increment to turn lefT"""
+        raise NotImplementedError("not expected to use?")
+    
+
+    def set_chalk(self, drawing: bool):
+        """sets the chalk to either be drawing or not drawing"""
+        if drawing == this.drawing:
+            return #can skip if same state.
+        #TODO: send chalk command to arduino
+        this.drawing = drawing
+
+
+
