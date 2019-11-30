1d91a670b0bac87917b6daaeffb3c64107f632c1
Scott Malonda <50493311+Pepuka@users.noreply.github.com>
2019-11-30 15:58:31 -0500

    Moved code into a function that uses Enums from common.py

--- vehiclePi/serialCommunication.py
+++ vehiclePi/serialCommunication.py
@@ -6,6 +6,7 @@ our system.  The serial port path should be set up manually to avoid sending
 data to the wrong serial port.
 
 Version 1.1: Updated running loop and set default serial port path to ttyUSB0
+Version 1.2: Moved code into a function for use by other scripts.
 
 Author: Scott Malonda
 Version: 1.1
@@ -13,42 +14,45 @@ Date: Nov 16, 2019
 """
 
 import serial
+from common import MOVE
 
-running = True
-waiting = False
-readyToMove = True
-received = 0
-toSend = 0
-
-arduino = serial.Serial(
-    port='/dev/ttyUSB0',
-    baudrate=9600
-    )
-if arduino.isOpen():
-    arduino.close()
-arduino.open()
-
-print("Input some movement instruction values in the format '49 50 21'.\n" +
-      "49, 51, and 52 (ASCII 0, 2, and 3) are the only expected values.")
-while running:
-    if readyToMove:
-        text = input(">>> ")
-        if (text == "stop" or text == "Stop"):
-            running = False
-            break
+def send_moves(arduinoPort = '/dev/ttyUSB0', *moves):
+    """
+    Sends a list of movement instructions to the vehicle Arduino via USB serial
+    connection.
     
-        # Parsing the command line arguments
-        nums = text.split(" ")
-        toSend = []
-        for i in range(0, len(nums)):
-            toSend.append(int(nums[i]))
+    Function assumes that the vehicle is ready to move when it is called, and
+    blocks until it receive a ready-to-move signal from the Arduino after the
+    instructions are set.
+    """
+    arduino = serial.Serial(
+        port=arduinoPort,
+        baudrate=9600
+        )
+    if arduino.isOpen():
+        arduino.close()
+    arduino.open()
+
+    waiting = False
+    received = 0
+    toSend = []
+    
+    # Parsing the moves argument
+    for i in range(0, len(moves)):
+        if (moves[i]==MOVE.F) toSend.append(48)
+        elif (moves[i]==MOVE.B) toSend.append(49)
+        elif (moves[i]==MOVE.R) toSend.append(50)
+        elif (moves[i]==MOVE.L) toSend.append(51)
+        elif (moves[i]==MOVE.FL) toSend.append(52)
+        elif (moves[i]==MOVE.FR) toSend.append(53)
+        elif (moves[i]==MOVE.BL) toSend.append(54)
+        elif (moves[i]==MOVE.BR) toSend.append(55)
         
 
-        # Sends the instructions that were inputted and waits to receive a message
-        # back from the Arduino
-        arduino.write(bytearray(toSend))
-        readyToMove = False
-        waiting = True
+    # Sends the instructions that were inputted and waits to receive a message
+    # back from the Arduino
+    arduino.write(bytearray(toSend))
+    waiting = True
 
     # Checks if the Arduino has sent us a message   
     while waiting:
@@ -57,6 +61,5 @@ while running:
             print(line)
             array = bytearray(line)
             if(array[0] == 6):
-                readyToMove = True
                 waiting = False
 
